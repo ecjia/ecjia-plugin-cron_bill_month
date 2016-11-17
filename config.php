ebb5a9126f14aa7df2a148a010250235ff39c8fe
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-17 09:22:22 +0800

    更新插件

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+return array(
+    'cron_code'      => 'cron_bill_month',
+	'forms' => array(
+		array('name' => 'bill_month_count', 'type' => 'select', 'value' => '5'),
+	),
+);
+
+// end
